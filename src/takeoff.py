9867e74bfbb97e00752b0c1570b222c254db0326
evana015 <evana015@googlemail.com>
2021-11-14 18:14:24 +0000

    Program to run alongside the simple.launch to get the drone to takeoff with comments as it is my first program, to be referred to later

---
+++ src/takeoff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Empty  # importing ros and empty msgs to publish takeoff command
+
+
+def takeoff():
+    pub = rospy.Publisher("takeoff", Empty, queue_size=10)  # node is publishing to the topic "takeoff" using empty type
+    rospy.init_node('drone',
+                    anonymous=True)  # initiates the node and gives it a name to communicate with the ROS master
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():  # Checks for a Ctrl-C or any other termination
+        pub.publish(Empty())    # Publishes Empty "{}" to the takeoff rostopic
+        rate.sleep()    # sleeps just long enough to maintain  the desired rate to loop through
+
+
+if __name__ == '__main__':
+    try:
+        takeoff()
+    except rospy.ROSInterruptException:
+        pass
