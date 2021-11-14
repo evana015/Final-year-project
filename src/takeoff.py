67d08edc74c14809ec5ad96b39ba11991a083636
evana015 <evana015@googlemail.com>
2021-11-14 18:16:14 +0000

    Altering topic as ros topic info takeoff returns unknown topic, topic is instead drone/takeoff

--- src/takeoff.py
+++ src/takeoff.py
@@ -4,7 +4,7 @@ from std_msgs.msg import Empty  # importing ros and empty msgs to publish takeof
 
 
 def takeoff():
-    pub = rospy.Publisher("takeoff", Empty, queue_size=10)  # node is publishing to the topic "takeoff" using empty type
+    pub = rospy.Publisher("drone/takeoff", Empty, queue_size=10)  # node is publishing to the topic "takeoff" using empty type
     rospy.init_node('drone',
                     anonymous=True)  # initiates the node and gives it a name to communicate with the ROS master
     rate = rospy.Rate(10)  # 10hz
