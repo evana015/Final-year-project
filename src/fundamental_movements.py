23a5b4d025f02999fc09a6ab58e94a2ccd53aed5
evana015 <evana015@googlemail.com>
2021-11-15 18:41:18 +0000

    Adding a stop command to move forward as it was going forever

--- src/fundamental_movements.py
+++ src/fundamental_movements.py
@@ -3,6 +3,7 @@ import rospy
 from std_msgs.msg import Empty  # importing ros and empty msgs to publish takeoff command
 from geometry_msgs.msg import Twist
 
+
 def takeoff():
     pub = rospy.Publisher("drone/takeoff", Empty, queue_size=10)  # node is publishing to the topic "takeoff" using
     # empty type
@@ -14,21 +15,30 @@ def takeoff():
             pub.publish((Empty()))  # Publishes Empty "{}" to the takeoff rostopic
             ctrl_c = True
         else:
-            rate.sleep()    # sleeps just long enough to maintain  the desired rate to loop through
+            rate.sleep()  # sleeps just long enough to maintain  the desired rate to loop through
 
 
 def move_straight():
-    pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=2)
 
     move_cmd = Twist()
     move_cmd.linear.x = 1.0
-
+    stop_moving_cmd = Twist()
+    stop_moving_cmd.linear.x = 0.0
     now = rospy.Time.now()
     rate = rospy.Rate(10)
 
     while rospy.Time.now() < now + rospy.Duration.from_sec(6):
         pub.publish(move_cmd)
         rate.sleep()
+    ctrl_c = False
+    while not ctrl_c:
+        connections = pub.get_num_connections()
+        if connections > 0:
+            pub.publish(stop_moving_cmd)
+            ctrl_c = True
+        else:
+            rate.sleep()
 
 
 if __name__ == '__main__':
