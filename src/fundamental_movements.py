cdc1ccd88a65005dc4567c7be82333e75dc73706
evana015 <evana015@googlemail.com>
2021-11-15 17:21:15 +0000

    Renaming file to include multiple functions to be made for other movements

--- src/fundamental_movements.py
+++ src/fundamental_movements.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import Empty  # importing ros and empty msgs to publish takeoff command
-
+from geometry_msgs.msg import Twist
 
 def takeoff():
     pub = rospy.Publisher("drone/takeoff", Empty, queue_size=10)  # node is publishing to the topic "takeoff" using
     # empty type
-    rospy.init_node('drone',
-                    anonymous=True)  # initiates the node and gives it a name to communicate with the ROS master
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():  # Checks for a Ctrl-C or any other termination
         pub.publish(Empty())    # Publishes Empty "{}" to the takeoff rostopic
         rate.sleep()    # sleeps just long enough to maintain  the desired rate to loop through
 
-
-if __name__ == '__main__':
     try:
+        rospy.init_node('drone',
+                        anonymous=True)  # initiates the node and gives it a name to communicate with the ROS master
         takeoff()
     except rospy.ROSInterruptException:
         pass
