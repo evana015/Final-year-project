cd1a588df6aa6a9586cbd9ff107b652fe88f8c39
evana015 <evana015@googlemail.com>
2021-12-02 02:25:43 +0000

    Adding first attempt at plotting script

---
+++ src/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+vertices = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (0, 0),
+]
+
+codes = []
+for i in range(0, len(vertices)):  # for the amount of vertices there are draw lines to them in order
+    codes.append(Path.LINETO)
+path = Path(vertices, codes)
+
+fig, ax = plt.subplots()
+patch = patches.PathPatch(path, facecolor='orange', lw=2)
+ax.add_patch(patch)
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+plt.show()
