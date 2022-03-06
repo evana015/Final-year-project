ffc95744e946d84b42985317714aec1f4f7da72a
evana015 <evana015@googlemail.com>
2022-03-06 20:44:08 +0000

    refactored takeoff and land. This was chosen to reduce duplicate code

--- src/code/drone_object.py
+++ src/code/drone_object.py
@@ -23,7 +23,12 @@ class Drone:
         # Publishers and Subscribers will be set with the following set as standard
         self.odom_sub = rospy.Subscriber("/drone/gt_pose", Pose, self.odom)
         self.movement_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+        self.takeoff_pub = rospy.Publisher("drone/takeoff", Empty, queue_size=1)
+        self.land_pub = rospy.Publisher("drone/land", Empty, queue_size=1)
 
+    # odom is the method that is assigned to be callback for the drone's subscriber to the "/drone/gt_pose" topic
+    # It takes a geometry_msgs.msg.Pose and breaks it down into the necessary components to update the drones location
+    # data. This allows the drone to be self aware of its position within the gazebo simulation
     def odom(self, msg):
         self.x = msg.position.x
         self.y = msg.position.y
@@ -32,17 +37,23 @@ class Drone:
         rot_q = msg.orientation
         (self.roll, self.pitch, self.theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
-    def takeoff(self):
-        pub = rospy.Publisher("drone/takeoff", Empty, queue_size=1)  # node is publishing to the topic "takeoff" using
-        # empty type
+    # takeoff_or_land behave as you would expect from the name
+    # Specifically the drone will publish a message to the topic "drone/takeoff" or "drone/land" with an Empty msg
+    # The method will take a "choice" parameter which will determine which publisher to use
+
+    def takeoff_or_land(self, choice):
         ctrl_c = False
+        if choice == "takeoff":
+            choice = self.takeoff_pub
+        else:
+            choice = self.land_pub
         while not ctrl_c:
-            connections = pub.get_num_connections()
+            connections = choice.get_num_connections()
             if connections > 0:
-                pub.publish((Empty()))  # Publishes Empty "{}" to the takeoff rostopic
+                self.choice.publish((Empty()))  # Publishes Empty "{}" to the takeoff rostopic
                 ctrl_c = True
             else:
-                self.rate.sleep()  # sleeps just long enough to maintain  the desired rate to loop through
+                self.rate.sleep()  # sleeps just long enough to maintain the desired rate to loop through
 
     def turn(self, goal_x, goal_y, speed):
         inc_x = goal_x - self.x
@@ -130,6 +141,7 @@ class Drone:
             self.movement_pub.publish(speed)
             r.sleep()
 
+
     def get_x(self):
         return self.x
 
@@ -150,5 +162,5 @@ class Drone:
 
 
 test_drone = Drone("Parrot", 10)
-test_drone.takeoff()
+test_drone.takeoff_or_land("takeoff")
 test_drone.move_to(4, 4, 0.5)
