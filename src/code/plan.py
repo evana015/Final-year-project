9f3b9ec6890040f19bacd336a2a43028e0a5e9a4
evana015 <evana015@googlemail.com>
2022-03-10 20:01:33 +0000

    Adding comments for future changes to be made using the concepts of splitting the map into rooms

--- src/code/plan.py
+++ src/code/plan.py
@@ -6,20 +6,21 @@ import math
 #               increase probability per iteration where not found
 #               add a weighted found chance (using probability found)
 #               take off and land must be in the plan but can be tested for on initiation (if hovering etc)
-#
+#               Take a dictionary/list of rooms that will be iterated through, each room will have an ideal pattern
+#               If not found move onto the next room, if last room and not found. Failed search and land
 #               implement flow chart as rules
 
 class Plan:
 
-    def __init__(self, boundary_width, boundary_length, probability):
-        self.boundary_width = boundary_width
-        self.boundary_length = boundary_length
+    def __init__(self, rooms, probability):
+        self.rooms = rooms
         self.probability = probability
         self.battery = 100
         self.in_flight = False
         self.actions = []
 
-    def reduce_battery(self, action, boundary_x=1, boundary_y=1):
+    def reduce_battery(self, action, boundary_x=1, boundary_y=1):  # current implementation infers max size of all
+        # rooms must be less than 80 units^2 or the battery will deplete mid plan
         switcher = {
             "take_off": 5,
             "land": 5,
@@ -30,7 +31,17 @@ class Plan:
         self.battery -= switcher.get(action, 0)
 
     def determine_found(self):
+        # randomise a number between 0 to 1 to 1 dp
+        # if random number is between 0 and self.probability then found
+        # if not self.probability += 0.1
         return None
 
     def populate_actions(self):
+        # set of rules aka the flow chart
+        return None
+
+    def iterate_rooms(self):
+        # go through room by room until found or exhausted
+        # rooms have the format [[boundary_x, boundary_y, bottom_left_co-ordinate AS (x,y)] ...]
+        # moving rooms costs battery so determine that
         return None
