be7a18fe5d73aadd964fb745869efa424ea1dc18
evana015 <evana015@googlemail.com>
2022-03-10 23:56:28 +0000

    adding movement battery reduction, testing move battery reduction and more edge cases of planning

--- src/code/plan.py
+++ src/code/plan.py
@@ -18,6 +18,7 @@ def reduce_battery(action, boundary_x=1, boundary_y=1):
     switcher = {
         "take_off": 5,
         "land": 5,
+        "move_to": boundary_x + boundary_y,
         "ess": boundary_x * boundary_y,
         "cls": boundary_x * boundary_y,
         "ps": boundary_x * boundary_y,
@@ -46,7 +47,7 @@ class Plan:
             self.probability += 0.1  # increase probability if room has been explored but target not been found
             return False
 
-    def populate_actions(self, boundary_x, boundary_y):
+    def populate_actions(self, boundary_x, boundary_y, move_to_x, move_to_y):
         area_of_room = boundary_x * boundary_y
         if area_of_room >= 35:  # determining if a room is of a large size using 35m^2 as the indicator of a large room
             if self.probability < 0.5:
@@ -58,7 +59,8 @@ class Plan:
                 action = "ess"
             else:
                 action = "ss"
-        reduction = reduce_battery(action, boundary_x, boundary_y)
+        movement_reduction = reduce_battery("move_to", move_to_x, move_to_y)
+        reduction = reduce_battery(action, boundary_x, boundary_y) + movement_reduction
         if self.battery - reduction <= 5:
             return "land"
         else:
@@ -67,7 +69,6 @@ class Plan:
 
     # go through room by room until found or exhausted
     # rooms have the format [[boundary_x, boundary_y, room_starting_x, room_starting_y] ...]
-    # TODO: moving rooms costs battery so determine that needs to be accounted for
     def create_plan(self):
         if not self.in_flight:
             if self.battery >= 10:
@@ -78,19 +79,20 @@ class Plan:
                 self.actions.append(["not enough battery to take off and safely land"])  # TODO: special case
 
         for room in self.rooms:
-            self.actions.append(["move_to", room[2], room[3]])
-            action = self.populate_actions(room[0], room[1])
+            action = self.populate_actions(room[0], room[1], room[2], room[3])
             if action == "land":
-                self.actions.append(["land"])
                 break
             elif self.determine_found():
+                self.actions.append(["move_to", room[2], room[3]])
                 self.actions.append([action, room[0], room[1]])
                 self.found = True
                 break
             else:
+                self.actions.append(["move_to", room[2], room[3]])
                 self.actions.append([action, room[0], room[1]])
 
         self.actions.append(["land"])
+        self.battery -= reduce_battery("land")
         self.in_flight = False
 
     def get_actions(self):
--- src/tests/test_plan.py
+++ src/tests/test_plan.py
@@ -7,6 +7,9 @@ def test_reduce_battery_takeoff():
     reduction = reduce_battery("take_off")
     assert reduction == 5
 
+def test_reduce_battery_move_to():
+    reduction = reduce_battery("move_to", 5, 9)
+    assert reduction == 14
 
 def test_reduce_battery_ess_and_cls():  # only one test required as ess,cls and ps calculate their area the same way
     reduction = reduce_battery("ess", 5, 5)
@@ -47,9 +50,9 @@ def test_single_ess_plan():
 
 
 def test_single_cls_plan():
-    new_plan = Plan([[15, 5, 8, 11]], 0.5, False)
+    new_plan = Plan([[14, 5, 8, 11]], 0.5, False)
     new_plan.create_plan()
-    assert new_plan.get_actions() == [["take_off"], ["move_to", 8, 11], ["cls", 15, 5], ["land"]]
+    assert new_plan.get_actions() == [["take_off"], ["move_to", 8, 11], ["cls", 14, 5], ["land"]]
 
 
 def test_single_ps_plan():
@@ -65,11 +68,29 @@ def test_single_ss_in_flight_plan():
 
 
 def test_cls_and_ss_plan():
-    new_plan = Plan([[7, 5, 0, 0], [2, 4, 3, 5]], 0.8, False)
+    new_plan = Plan([[7, 5, 0, 0], [2, 4, 1, 6]], 0.8, False)
     new_plan.create_plan()
-    assert new_plan.get_actions() == [["take_off"], ["move_to", 0, 0], ["cls", 7, 5], ["move_to", 3, 5], ["ss", 2, 4],
-                                      ["land"]] or [["take_off"], ["move_to", 0, 0], ["cls", 7, 5], ["land"]]
+    assert new_plan.get_actions() == [["take_off"], ["move_to", 0, 0], ["cls", 7, 5], ["move_to", 1, 6], ["ss", 2, 4],
+                                      ["land"]] \
+           or new_plan.get_actions() == [["take_off"], ["move_to", 0, 0], ["cls", 7, 5], ["land"]]
 
+def test_ps_and_ess_plan():
+    new_plan = Plan([[6, 7, 2, 4], [6, 1, 8, 5]], 0.3, False)
+    new_plan.create_plan()
+    assert new_plan.get_actions() == [["take_off"], ["move_to", 2, 4], ["ps", 6, 7], ["move_to", 8, 5], ["ess", 6, 1],
+                                      ["land"]] \
+           or new_plan.get_actions() == [["take_off"], ["move_to", 2, 4], ["ps", 6, 7], ["land"]]
+
+def test_ess_and_ss_plan():  # tests if the probability is incremented as intended
+    new_plan = Plan([[2, 5, 0, 0],[3, 1, 2, 3]], 0.4, True)
+    new_plan.create_plan()
+    assert new_plan.get_actions() == [["move_to", 0, 0], ["ess", 2, 5], ["move_to", 2, 3], ["ss", 3, 1], ["land"]] \
+           or [["move_to", 0, 0], ["ess", 2, 5], ["land"]]
+
+def test_plan_insufficient_battery():
+    new_plan = Plan([[18, 5, 0, 0]], 0.7, False)
+    new_plan.create_plan()
+    assert new_plan.get_actions() == [["take_off"], ["land"]]
     # TODO: test battery running out and more edge cases
 
 # determine_found generates a random float between 0 to 1 and return a boolean based on comparing to see if its in
