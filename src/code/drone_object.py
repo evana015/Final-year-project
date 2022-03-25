95a8a1f40a09d90ef97c2b1010dc0701caa47bb4
evana015 <evana015@googlemail.com>
2022-03-25 10:41:01 +0000

    reformatting the terminal input to be in accordance with PEP-8

--- src/code/drone_object.py
+++ src/code/drone_object.py
@@ -278,8 +278,8 @@ def main():
     partitions = []
     for i in range(0, number_of_partitions):
         print("Partition ", i)
-        parameters = input("Please input the dimensions of the partition and the co-ordinate of the lower left corner in"
-                           " the format width,length,x,y: ")
+        parameters = input("Please input the dimensions of the partition and the co-ordinate of the lower left corner "
+                           "in the format width,length,x,y: ")
         partitions.append([int(n) for n in parameters.split(",")])
     probability = float(input("Please enter the probability of the object being found between 0 (unlikely) and 1 ("
                               "likely): "))
